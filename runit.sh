60796a1eb127639ef66218d27c5d76e02d47ed21
Stephanie <durandsl@mymail.vcu.edu>
2016-11-17 14:57:34 -0500

    committing changes to runit script

--- runit.sh
+++ runit.sh
@@ -6,12 +6,15 @@
 d=100
 
 #specify all of the files to use
-train_file=data/train60
-test_file=data/test40
+train_file_2013=data/train/clean.tsv
+dev_file_2013=data/dev/clean.tsv
+train_file_2016=data/2016/clean_train.tsv
+dev_file_2016=data/2016/clean_dev.tsv
+test_file=data/2016/clean_devtest.tsv
 out_file=predictions.out
 
 #train the machine learning classifiers
-python code/train.py $d $train_file
+#python code/train.py $d $train_file_2013 $dev_file_2013 $train_file_2016 $dev_file_2016
 
 #classify the test data
 python code/run_classifier.py $d $out_file $test_file
